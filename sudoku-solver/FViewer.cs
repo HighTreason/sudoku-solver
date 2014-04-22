93026651b5e938aba706f04d4d076b4ff96f5251
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2014-04-22 15:44:30 +0300

    initial develop commit ...&...

---
+++ sudoku-solver/FViewer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sudoku_solver
+{
+    public partial class FViewer : Form
+    {
+        public FViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
